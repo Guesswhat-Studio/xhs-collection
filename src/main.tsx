7332a798187791cb50a7b65a863e4b749a25b504
YuyangXue_Ed <Yuyang.xue@ed.ac.uk>
2026-06-01 00:03:02 +0100

    Bundle desktop font schemes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
+import '@fontsource-variable/noto-sans-sc/index.css';
+import '@fontsource-variable/noto-serif-sc/index.css';
+import 'lxgw-wenkai-screen-webfont/lxgwwenkaigbscreen.css';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
